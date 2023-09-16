e6310c270d1e8eca33cdb5c196634477d667b386
Jan <jan@seifert-online.de>
2023-09-16 23:00:18 +0200

    added workflow image

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ if __name__ == "__main__":
         userinput = input(">>> ")
         if config["timestamp"]=="yes":
             print(time.strftime("[%H:%M:%S]"))
-        response = interact(userinput, conversation)
+        response = interact(userinput, conversation, verbose=True)
         prefix = time.strftime("[%H:%M:%S] ") if config["timestamp"]=="yes" else ""
         print(prefix + response)
